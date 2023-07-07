74212446269961459b6565fea71b9c716ae0e11e
ThomasHuynh <Thomas_TTH@live.com>
2023-07-07 12:36:14 -0400

    added general styling and implemented nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "../scss/_nav.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SwitchLogo from '../assets/SwitchLogo.svg';
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="navContainer">
+        <a href="/">
+          <img src={SwitchLogo} alt="" className="logo" />
+        </a>
+
+        <ul className="navLinks">
+
+          <li className="navList">
+            <a href="/" className="navLink">
+              Home
+            </a>
+          </li>
+
+          <li className="navList">
+            <a href="/" className="navLink">
+              Games
+            </a>
+          </li>
+
+          {/* <button className="buttonMenu">
+            <FontAwesomeIcon icon="bars" />
+          </button> */}
+
+          <li className="navCart">
+            <a href="/cart">
+              <FontAwesomeIcon icon="shopping-cart" />
+            </a>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faShoppingCart)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
