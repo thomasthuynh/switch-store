e6ff27292628b3610fc270484b916f654755ddb1
ThomasHuynh <Thomas_TTH@live.com>
2023-07-24 15:56:33 -0400

    added recommended games below selected game

--- src/pages/GameInfo.jsx
+++ src/pages/GameInfo.jsx
@@ -5,16 +5,16 @@ import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Rating from "../components/ui/Rating";
 import Price from "../components/ui/Price";
+import Game from "../components/ui/Game";
 
 const GameInfo = () => {
-
   const { id } = useParams();
 
   const game = gameData.find((title) => {
     return +title.id === +id;
-  })
+  });
 
-  console.log(game)
+  console.log(game);
 
   return (
     <div className="gamesBody">
@@ -30,17 +30,11 @@ const GameInfo = () => {
 
             <div className="gameSelected">
               <figure className="gameSelectedFigure">
-                <img
-                  src={game.url}
-                  alt=""
-                  className="gameSelectedImg"
-                />
+                <img src={game.url} alt="" className="gameSelectedImg" />
               </figure>
 
               <div className="gameSelectedDescription">
-                <h2 className="gameSelectedTitle">
-                  {game.title}
-                </h2>
+                <h2 className="gameSelectedTitle">{game.title}</h2>
 
                 <Rating rating={game.rating} className="gameRating" />
                 <Price
@@ -52,9 +46,7 @@ const GameInfo = () => {
                 <div className="gameSummary">
                   <h3 className="gameSummaryTitle">Summary</h3>
 
-                  <p className="gameSummaryOverview">
-                    {game.overview}
-                  </p>
+                  <p className="gameSummaryOverview">{game.overview}</p>
                 </div>
 
                 <button className="btn">Add to Cart</button>
@@ -63,10 +55,20 @@ const GameInfo = () => {
           </div>
         </div>
 
-        <div className="recommendedBooksContainer">
+        <div className="recommendedGamesContainer">
           <div className="wrapper">
             <div className="gameSelectedTop">
-              <h2 className="recommendedBooksTitle">Recommended Books</h2>
+              <h2 className="recommendedGamesTitle">Recommended Games</h2>
+            </div>
+            <div className="recommendedGames">
+              {gameData
+                .filter((game) => {
+                  return game.rating === 5 && +game.id !== +id;
+                })
+                .slice(0, 4)
+                .map((game) => {
+                  return <Game game={game} key={game.id} />;
+                })}
             </div>
           </div>
         </div>
